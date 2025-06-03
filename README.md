c3573b260011e94e1d16f5c4f609aa2b8686aaf9
Abdul Rehman <welaboy101@gmail.com>
2025-06-03 09:47:18 +0500

    Add init config with pen tracking and data point storage with correct pens

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # drawtui
+
+A simple Terminal base Writing Board.
+
+If you are showing someone something and want to draw a quick sketch, this is the tool for you.
+
+This tool uses FTXUI to render a simple canvas with several pen options. 
+
+## Installation
+To use this just clone this repository and run `cmake ./` and then `make`.
+
+Enjoy and let me know if you have any issues or suggestions :)
+
---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include "ftxui/component/component.hpp"
+#include "ftxui/component/component_base.hpp"
+#include "ftxui/component/event.hpp"
+#include "ftxui/component/mouse.hpp"
+#include "ftxui/component/screen_interactive.hpp"
+#include "ftxui/dom/canvas.hpp"
+#include "ftxui/screen/color.hpp"
+
+int main()
+{
+    using namespace ftxui;
+
+    int mouse_x = 0;
+    int mouse_y = 0;
+    bool is_drawing = false;
+
+    std::vector<std::pair<std::pair<int, int>, int>> clicked_points; //{(x,y), 0} coordinates and choice number
+
+    auto simple_pen = Renderer([&]
+                               {
+    auto c = Canvas(300, 300);
+    c.DrawText(0, 0, "Simple pen");
+    for (size_t i = 0; i < clicked_points.size(); i++)
+    {
+        if(clicked_points[i].second == 0)
+        c.DrawPoint(clicked_points[i].first.first, clicked_points[i].first.second, true,Color::Blue3);
+        else if (clicked_points[i].second == 1)
+        c.DrawBlockCircle(clicked_points[i].first.first, clicked_points[i].first.second, true,Color::Blue3);
+    }
+    return canvas(std::move(c)); });
+
+    auto thick_pen = Renderer([&]
+                              {
+    auto c = Canvas(300, 300);
+    c.DrawText(0, 0, "Thick pen");
+    for (size_t i = 0; i < clicked_points.size(); i++)
+    {
+        if(clicked_points[i].second == 1)
+        c.DrawBlockCircle(clicked_points[i].first.first, clicked_points[i].first.second, true,Color::Blue3);
+        else if(clicked_points[i].second == 0)
+        c.DrawPoint(clicked_points[i].first.first, clicked_points[i].first.second, true,Color::Blue3);
+    }
+    return canvas(std::move(c)); });
+
+    int selected_tab = 0;
+    auto tab = Container::Tab(
+        {simple_pen,
+         thick_pen},
+        &selected_tab);
+
+    auto tab_with_mouse = CatchEvent(tab, [&](Event e)
+                                     {
+        if (e.is_mouse() && e.mouse().x < 130)
+        {
+            
+
+            mouse_x = (e.mouse().x - 1) * 2;
+            mouse_y = (e.mouse().y - 1) * 4;
+
+            if (e.mouse().button == Mouse::Button::Left)
+            {
+                if (e.mouse().motion == Mouse::Motion::Pressed)
+                {
+                    is_drawing = true;
+                    if(selected_tab == 0)
+                    clicked_points.push_back({{mouse_x, mouse_y},0});
+                    else if (selected_tab == 1)
+                    clicked_points.push_back({{mouse_x, mouse_y},1});
+                    return true;
+                }
+                else if (e.mouse().motion == Mouse::Motion::Released)
+                {
+                    is_drawing = false;
+                    return true;
+                }
+            }
+            if (is_drawing && e.mouse().motion == Mouse::Motion::Pressed)
+            {
+                if(selected_tab == 0)
+                    clicked_points.push_back({{mouse_x, mouse_y},0});
+                else if (selected_tab == 1)
+                    clicked_points.push_back({{mouse_x, mouse_y},1});
+                return true;
+            }
+        
+        }
+    return false; });
+
+    std::vector<std::string> tab_titles = {
+        "Simple Pen",
+        "Thick Pen"};
+    auto tab_toggle = Menu(&tab_titles, &selected_tab);
+
+    auto component = Container::Horizontal({tab_toggle,
+                                            tab_with_mouse});
+
+    auto component_renderer = Renderer(component, [&]
+                                       { return hbox({tab_with_mouse->Render(),
+                                                      separator(),
+                                                      tab_toggle->Render() | size(WIDTH, EQUAL, 30)}) |
+                                                border; });
+
+    auto screen = ScreenInteractive::FitComponent();
+    screen.Loop(component_renderer);
+
+    return 0;
+}
