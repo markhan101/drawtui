c6653a80149c85271a130a9464c8f535495867bd
Abdul Rehman <welaboy101@gmail.com>
2025-06-05 03:32:06 +0500

    Added Eraser functionality and cleaned up some code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <memory>
 #include <string>
+#include <algorithm>
 
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
@@ -14,21 +15,18 @@
 #include "ftxui/screen/color.hpp"
 #include "ftxui/component/captured_mouse.hpp"
 
-using namespace ftxui;
-
-Element ColorTile(int red, int green, int blue)
+ftxui::Element ColorTile(int red, int green, int blue)
 {
-    return text("") | size(WIDTH, GREATER_THAN, 14) |
-           size(HEIGHT, GREATER_THAN, 7) | bgcolor(Color::RGB(red, green, blue));
+    return ftxui::text("") | ftxui::size(ftxui::WIDTH, ftxui::GREATER_THAN, 14) |
+           ftxui::size(ftxui::HEIGHT, ftxui::GREATER_THAN, 7) | ftxui::bgcolor(ftxui::Color::RGB(red, green, blue));
 }
 
-Element ColorString(int red, int green, int blue)
+ftxui::Element ColorString(int red, int green, int blue)
 {
-    return text("RGB = (" +                   //
-                std::to_string(red) + "," +   //
-                std::to_string(green) + "," + //
-                std::to_string(blue) + ")"    //
-    );
+    return ftxui::text("RGB = (" +
+                       std::to_string(red) + "," +
+                       std::to_string(green) + "," +
+                       std::to_string(blue) + ")");
 }
 
 void set_default_colours(int selection, int *red, int *green, int *blue)
@@ -40,27 +38,27 @@ void set_default_colours(int selection, int *red, int *green, int *blue)
         *green = 0;
         *blue = 0;
         break;
-    case 1: // Blue
+    case 1:
         *red = 0;
         *green = 0;
         *blue = 255;
         break;
-    case 2: // Green
+    case 2:
         *red = 0;
         *green = 255;
         *blue = 0;
         break;
-    case 3: // Yellow
+    case 3:
         *red = 255;
         *green = 255;
         *blue = 0;
         break;
-    case 4: // Orange
+    case 4:
         *red = 255;
         *green = 165;
         *blue = 0;
         break;
-    case 5: // White
+    case 5:
         *red = 255;
         *green = 255;
         *blue = 255;
@@ -73,38 +71,44 @@ void set_default_colours(int selection, int *red, int *green, int *blue)
 class Clicked_Point
 {
 private:
-    std::pair<int, int> coord;
+    std::pair<int,
+              int>
+        coord;
     int tab_option;
-    int red, green, blue;
+    int red,
+        green,
+        blue;
 
 public:
-    Clicked_Point(std::pair<int, int> c, int t_o, int r = 50, int g = 90, int b = 200) : coord(c), tab_option(t_o), red(r), green(g), blue(b)
-    {
-    }
+    Clicked_Point(std::pair<int, int> c, int t_o, int r = 50, int g = 90, int b = 200) : coord(c),
+                                                                                         tab_option(t_o),
+                                                                                         red(r),
+                                                                                         green(g),
+                                                                                         blue(b) {}
 
-    int get_tab_option()
+    int get_tab_option() const
     {
         return tab_option;
     }
 
-    int get_red()
+    int get_red() const
     {
         return red;
     }
-    int get_green()
+    int get_green() const
     {
         return green;
     }
-    int get_blue()
+    int get_blue() const
     {
         return blue;
     }
 
-    int get_x()
+    int get_x() const
     {
         return coord.first;
     }
-    int get_y()
+    int get_y() const
     {
         return coord.second;
     }
@@ -122,20 +126,18 @@ int main()
     int green = 0;
     int blue = 0;
 
-    std::vector<std::pair<std::pair<int, int>, int>> clicked_points; //{(x,y), 0} coordinates and choice number
     std::vector<Clicked_Point> cp;
 
     Component simple_pen = Renderer([&]
                                     {
     Canvas c = Canvas(300, 300);
     c.DrawText(0, 0, "Simple pen");
-    for (size_t i = 0; i < cp.size(); i++)
-    {
-        if(cp[i].get_tab_option() == 0)
-        c.DrawPoint(cp[i].get_x(), cp[i].get_y(), true,Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
-        
-        else if (cp[i].get_tab_option() == 1)
-        c.DrawBlockCircle(cp[i].get_x(), cp[i].get_y(), true,Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
+    for (size_t i = 0; i < cp.size(); i++) {
+      if (cp[i].get_tab_option() == 0)
+        c.DrawPoint(cp[i].get_x(), cp[i].get_y(), true, Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
+
+      else if (cp[i].get_tab_option() == 1)
+        c.DrawBlockCircle(cp[i].get_x(), cp[i].get_y(), true, Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
     }
     return canvas(std::move(c)); });
 
@@ -143,65 +145,135 @@ int main()
                                    {
     Canvas c = Canvas(300, 300);
     c.DrawText(0, 0, "Thick pen");
-    for (size_t i = 0; i < cp.size(); i++)
-    {
-        if(cp[i].get_tab_option() == 0)
-        c.DrawPoint(cp[i].get_x(), cp[i].get_y(), true,Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
-        else if (cp[i].get_tab_option() == 1)
-        c.DrawBlockCircle(cp[i].get_x(), cp[i].get_y(), true,Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
+    for (size_t i = 0; i < cp.size(); i++) {
+      if (cp[i].get_tab_option() == 0)
+        c.DrawPoint(cp[i].get_x(), cp[i].get_y(), true, Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
+      else if (cp[i].get_tab_option() == 1)
+        c.DrawBlockCircle(cp[i].get_x(), cp[i].get_y(), true, Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
     }
     return canvas(std::move(c)); });
 
     Component clear_board = Renderer([&]
                                      {
-                                         Canvas c = Canvas(300, 300);
-                                         c.DrawText(0, 0, "Board Cleared!");
-                                         cp.clear();
-                                         return canvas(std::move(c)); });
+    Canvas c = Canvas(300, 300);
+    c.DrawText(0, 0, "Board Cleared!");
+    cp.clear();
+    return canvas(std::move(c)); });
+
+    Component eraser = Renderer([&]
+                                {
+    Canvas c = Canvas(300, 300);
+
+    for (size_t i = 0; i < cp.size(); i++) {
+
+      if (cp[i].get_tab_option() == 0)
+        c.DrawPoint(cp[i].get_x(), cp[i].get_y(), true, Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
+      else if (cp[i].get_tab_option() == 1)
+        c.DrawBlockCircle(cp[i].get_x(), cp[i].get_y(), true, Color::RGB(cp[i].get_red(), cp[i].get_green(), cp[i].get_blue()));
+
+    }
+    return canvas(std::move(c)); });
 
     int selected_tab = 0;
     int radio_selected = 0;
     int prev_radio_selected = 0;
-    Component tab = Container::Tab(
-        {simple_pen,
-         thick_pen,
-         clear_board},
-        &selected_tab);
+    Component tab = Container::Tab({simple_pen,
+                                    thick_pen,
+                                    eraser,
+                                    clear_board},
+                                   &selected_tab);
 
     Component tab_with_mouse = CatchEvent(tab, [&](Event e)
                                           {
-        if (e.is_mouse() && e.mouse().x < 130)
-        {
-        mouse_x = (e.mouse().x - 1) * 2;
-        mouse_y = (e.mouse().y - 1) * 4;
+    if (e.is_mouse() && e.mouse().x < 130) {
+      mouse_x = (e.mouse().x - 1) * 2;
+      mouse_y = (e.mouse().y - 1) * 4;
 
-        if (e.mouse().button == Mouse::Button::Left)
-        {
-            if (e.mouse().motion == Mouse::Motion::Pressed)
-            {
-                is_drawing = true;
-                if (selected_tab == 0)
-                    cp.push_back({{mouse_x, mouse_y}, 0, red, green, blue});
-                else if (selected_tab == 1)
-                    cp.push_back({{mouse_x, mouse_y}, 1, red, green, blue});
-                return true;
+      if (e.mouse().button == Mouse::Button::Left) {
+        if (e.mouse().motion == Mouse::Motion::Pressed) {
+          is_drawing = true;
+          if (selected_tab == 0)
+            cp.push_back({
+              {
+                mouse_x,
+                mouse_y
+              },
+              0,
+              red,
+              green,
+              blue
+            });
+          else if (selected_tab == 1)
+            cp.push_back({
+              {
+                mouse_x,
+                mouse_y
+              },
+              1,
+              red,
+              green,
+              blue
+            });
+          else if (selected_tab == 2) {
+
+            auto it = std::find_if(cp.begin(), cp.end(),
+              [mouse_x, mouse_y](const Clicked_Point & point) {
+                return point.get_x() == mouse_x && point.get_y() == mouse_y;
+              });
+
+            if (it != cp.end()) {
+              cp.erase(it);
+
             }
-            else if (e.mouse().motion == Mouse::Motion::Released)
+          }
+          return true;
+        } else if (e.mouse().motion == Mouse::Motion::Released) {
+          is_drawing = false;
+          return true;
+        }
+      }
+      if (is_drawing && e.mouse().motion == Mouse::Motion::Pressed) {
+        if (selected_tab == 0)
+          cp.push_back({
             {
-                is_drawing = false;
-                return true;
-            }
-        }
-        if (is_drawing && e.mouse().motion == Mouse::Motion::Pressed)
-        {
-            if (selected_tab == 0)
-                cp.push_back({{mouse_x, mouse_y}, 0, red, green, blue});
-            else if (selected_tab == 1)
-                cp.push_back({{mouse_x, mouse_y}, 1, red, green, blue});
-            return true;
-        }
+              mouse_x,
+              mouse_y
+            },
+            0,
+            red,
+            green,
+            blue
+          });
+        else if (selected_tab == 1)
+          cp.push_back({
+            {
+              mouse_x,
+              mouse_y
+            },
+            1,
+            red,
+            green,
+            blue
+          });
+        else if (selected_tab == 2) {
+
+          //get iterator for the point
+          auto it = std::find_if(cp.begin(), cp.end(),
+            [mouse_x, mouse_y](const Clicked_Point & point) {
+              return point.get_x() == mouse_x && point.get_y() == mouse_y;
+            });
+
+          //remove point
+          if (it != cp.end()) {
+            cp.erase(it);
+
+          }
 
         }
+        return true;
+      }
+
+    }
     return false; });
 
     Component slider_red = Slider("Red  :", &red, 0, 255, 1);
@@ -217,10 +289,16 @@ int main()
     std::vector<std::string> tab_titles = {
         "Simple Pen",
         "Thick Pen",
+        "Eraser",
         "Clear Board"};
 
     std::vector<std::string> radio_colour_options = {
-        "Red", "Blue", "Green", "Yellow", "Orange", "White"};
+        "Red",
+        "Blue",
+        "Green",
+        "Yellow",
+        "Orange",
+        "White"};
 
     Component radiobox_colours = Radiobox(&radio_colour_options, &radio_selected);
 
@@ -232,35 +310,33 @@ int main()
                                                       slider_container});
 
     Component component_renderer = Renderer(main_component, [&]
-                                            { 
-                                                
-                                                if(radio_selected != prev_radio_selected)
-                                                {
-                                                    set_default_colours(radio_selected, &red, &green, &blue);
-                                                    prev_radio_selected = radio_selected;
-                                                }
-                                                
-                                                
-                                                return hbox({
-                                                         tab_with_mouse->Render(),
-                                                         separator(),
-                                                         vbox({
-                                                             tab_toggle->Render(),
-                                                             separator(),
-                                                             ColorTile(red, green, blue),
-                                                             separator(),
-                                                             radiobox_colours->Render(),
-                                                             separator(),
-                                                             slider_red->Render(),
-                                                             separator(),
-                                                             slider_green->Render(),
-                                                             separator(),
-                                                             slider_blue->Render(),
-                                                             separator(),
-                                                             ColorString(red, green, blue),
-                                                         }) | size(WIDTH, EQUAL, 30),
-                                                     }) |
-                                                     border; });
+                                            {
+
+    if (radio_selected != prev_radio_selected) {
+      set_default_colours(radio_selected, & red, & green, & blue);
+      prev_radio_selected = radio_selected;
+    }
+
+    return hbox({
+        tab_with_mouse -> Render(),
+        separator(),
+        vbox({
+          tab_toggle -> Render(),
+          separator(),
+          ColorTile(red, green, blue),
+          separator(),
+          radiobox_colours -> Render(),
+          separator(),
+          slider_red -> Render(),
+          separator(),
+          slider_green -> Render(),
+          separator(),
+          slider_blue -> Render(),
+          separator(),
+          ColorString(red, green, blue),
+        }) | size(WIDTH, EQUAL, 30),
+      }) |
+      border; });
     ScreenInteractive screen = ScreenInteractive::FitComponent();
     screen.Loop(component_renderer);
 
